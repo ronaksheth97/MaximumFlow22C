d8377b2e1733ca5675d4c0eb12859e2c75cbabb9
ronaksheth97 <ronaksheth97@gmail.com>
2016-06-09 09:54:01 -0700

    Revert "Revert "Updated .4""
    This reverts commit 03ca51b7ca2ad9c19ba32fe2b33d9676d3280e2b.

--- MaximumFlowProject/src/Main.java
+++ MaximumFlowProject/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args){
-		Executable program = new Executable(); // Calls init() from Executable's default Constructor
+		Executable<String> program = new Executable<String>(); // Calls init() from Executable's default Constructor
 											  // To open main menu
 	}
 }
